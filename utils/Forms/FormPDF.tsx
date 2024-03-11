92002544f90f31d740817940e61b54c4465015cb
Kayla <kayla.pattern@gmail.com>
2024-03-11 02:07:51 -0700

    Added back button on forms page

--- utils/Forms/FormPDF.tsx
+++ utils/Forms/FormPDF.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { StyleSheet, Dimensions, View } from 'react-native';
+import { StyleSheet, Dimensions, View, TouchableOpacity, Text } from 'react-native';
 import { WebView } from "react-native-webview";
 
-export default function FormPDF() {
+export default function FormPDF({navigation}) {
     const source = require('../../assets/faketaxform.pdf')
     return (
         <View style={styles.container}>
             <WebView
                 source={source}
                 style={styles.pdf}/>
+            <TouchableOpacity
+                style={styles.backButton}
+                onPress={() => navigation.navigate('BrowseMain')} 
+            >
+                <Text style={styles.backButtonText}>Go Back</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -24,6 +30,13 @@ const styles = StyleSheet.create({
         flex:1,
         width:Dimensions.get('window').width,
         height:Dimensions.get('window').height,
-    }
+    },
+    backButton: {
+        marginBottom: 20,
+    },
+    backButtonText: {
+        color: '#5A6F72',
+        fontSize: 18,
+    },
 });
 
